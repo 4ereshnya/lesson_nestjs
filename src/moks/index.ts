1783233b4774e9093b814b43421011508aa3535d
Kirill <visa5064@gmail.com>
2023-12-30 16:11:13 +0400

    Init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+    {
+      "_id": "658af4a7fc7c0b0a84782f50",
+      "index": 0,
+      "guid": "e92dbbbb-fc27-4f8b-9824-40c449ad0326",
+      "isActive": false,
+      "balance": "$1,481.18",
+      "picture": "http://placehold.it/32x32",
+      "age": 28,
+      "eyeColor": "brown",
+      "name": "Lupe Bruce",
+      "gender": "female",
+      "company": "MICRONAUT",
+      "email": "lupebruce@micronaut.com",
+      "phone": "+1 (944) 530-2418",
+      "address": "882 Havens Place, Twilight, Florida, 6599",
+      "about": "Velit elit adipisicing amet ex proident nostrud do consequat amet laborum fugiat consectetur exercitation irure. Do ex commodo ullamco officia aute ea qui consectetur exercitation culpa. Reprehenderit fugiat sunt cillum exercitation reprehenderit.\r\n",
+      "registered": "2019-04-14T03:30:46 -04:00",
+      "latitude": 55.728399,
+      "longitude": 11.412509,
+      "tags": [
+        "anim",
+        "incididunt",
+        "enim",
+        "reprehenderit",
+        "velit",
+        "minim",
+        "velit"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Ladonna Shields"
+        },
+        {
+          "id": 1,
+          "name": "Sherry Byrd"
+        },
+        {
+          "id": 2,
+          "name": "Aileen Manning"
+        }
+      ],
+      "greeting": "Hello, Lupe Bruce! You have 9 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "658af4a7157014898fa21c79",
+      "index": 1,
+      "guid": "9d962e69-0b60-49bb-bf9c-c2b0f8debb0d",
+      "isActive": false,
+      "balance": "$2,795.33",
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "green",
+      "name": "Martinez Cobb",
+      "gender": "male",
+      "company": "AQUASSEUR",
+      "email": "martinezcobb@aquasseur.com",
+      "phone": "+1 (938) 595-2937",
+      "address": "721 Crawford Avenue, Oretta, Puerto Rico, 9542",
+      "about": "Amet ipsum incididunt enim ex non sit magna mollit nostrud consequat id aliquip nulla dolore. Qui aliqua cillum non nisi sit in. Occaecat officia eu labore enim culpa. Aute minim incididunt ex magna aliquip est labore nostrud qui ad ex nulla. Exercitation officia elit quis laboris eu duis aliqua dolore excepteur velit veniam aute. Ad mollit ea enim ut velit sint magna est mollit ipsum aute nisi magna. Ut veniam reprehenderit est incididunt adipisicing cupidatat cillum sunt aute.\r\n",
+      "registered": "2023-12-02T01:07:58 -04:00",
+      "latitude": 22.917207,
+      "longitude": -102.747085,
+      "tags": [
+        "ex",
+        "dolore",
+        "est",
+        "magna",
+        "excepteur",
+        "voluptate",
+        "pariatur"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Maryellen French"
+        },
+        {
+          "id": 1,
+          "name": "Robin Ortega"
+        },
+        {
+          "id": 2,
+          "name": "Lena Hooper"
+        }
+      ],
+      "greeting": "Hello, Martinez Cobb! You have 6 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "658af4a7a2cebeecc92dc130",
+      "index": 2,
+      "guid": "70bd6575-f413-4661-a560-eb63ed067dea",
+      "isActive": true,
+      "balance": "$2,743.84",
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "green",
+      "name": "Wendi Goff",
+      "gender": "female",
+      "company": "QUOTEZART",
+      "email": "wendigoff@quotezart.com",
+      "phone": "+1 (827) 504-3152",
+      "address": "385 Court Square, Slovan, American Samoa, 9899",
+      "about": "Velit id et consequat laboris tempor nulla consectetur nulla enim voluptate ex ex velit ipsum. Quis mollit qui esse ullamco excepteur ad aliqua commodo. Qui nulla ullamco amet ullamco eiusmod est eiusmod consectetur amet veniam incididunt est enim Lorem. Aute id do id minim minim nisi nulla quis qui ex in. Id sint Lorem voluptate duis ut et velit ea enim quis enim ipsum labore ad. In cupidatat cillum ullamco enim consequat excepteur culpa duis pariatur deserunt.\r\n",
+      "registered": "2023-03-21T08:55:54 -04:00",
+      "latitude": 33.018904,
+      "longitude": -78.567695,
+      "tags": [
+        "commodo",
+        "ullamco",
+        "et",
+        "ut",
+        "enim",
+        "commodo",
+        "sit"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Carolina Merrill"
+        },
+        {
+          "id": 1,
+          "name": "Alma Hayes"
+        },
+        {
+          "id": 2,
+          "name": "Tami Rich"
+        }
+      ],
+      "greeting": "Hello, Wendi Goff! You have 1 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "658af4a7d742acdbfc2148ea",
+      "index": 3,
+      "guid": "2d52a495-d958-47fa-aa91-2b509b081d80",
+      "isActive": false,
+      "balance": "$2,831.81",
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Susan Riley",
+      "gender": "female",
+      "company": "SLUMBERIA",
+      "email": "susanriley@slumberia.com",
+      "phone": "+1 (852) 561-2427",
+      "address": "461 Mayfair Drive, Dixie, South Dakota, 9658",
+      "about": "Ullamco minim exercitation proident officia velit aliquip laborum enim. Veniam dolore dolor nisi excepteur. Dolore exercitation adipisicing eiusmod aliquip veniam proident ad in do.\r\n",
+      "registered": "2014-12-14T05:17:04 -03:00",
+      "latitude": -62.897008,
+      "longitude": -109.116038,
+      "tags": [
+        "incididunt",
+        "consequat",
+        "enim",
+        "consequat",
+        "in",
+        "nulla",
+        "eiusmod"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lester Dunlap"
+        },
+        {
+          "id": 1,
+          "name": "Tonya Wright"
+        },
+        {
+          "id": 2,
+          "name": "Debora Donaldson"
+        }
+      ],
+      "greeting": "Hello, Susan Riley! You have 5 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "658af4a7de1d4a9bb68afb02",
+      "index": 4,
+      "guid": "1f196ac7-7de5-43b4-b241-d6cb59a79cec",
+      "isActive": true,
+      "balance": "$1,928.33",
+      "picture": "http://placehold.it/32x32",
+      "age": 27,
+      "eyeColor": "green",
+      "name": "Rivas Wagner",
+      "gender": "male",
+      "company": "ENQUILITY",
+      "email": "rivaswagner@enquility.com",
+      "phone": "+1 (834) 582-2209",
+      "address": "540 Coffey Street, Cade, Arizona, 3714",
+      "about": "Cupidatat ipsum eu et sunt irure sunt id esse sunt non proident pariatur magna. Velit laboris officia officia in excepteur veniam mollit non excepteur. Non cupidatat sunt proident in elit nisi. Esse duis cillum reprehenderit in nisi sunt esse ad ea sit velit et. Qui fugiat eu sunt amet exercitation nisi sint sint. Minim laborum non exercitation dolore laboris officia id culpa adipisicing. Est non proident reprehenderit commodo officia cillum excepteur duis.\r\n",
+      "registered": "2018-11-17T07:09:29 -04:00",
+      "latitude": -71.17484,
+      "longitude": 111.734376,
+      "tags": [
+        "enim",
+        "aute",
+        "laborum",
+        "enim",
+        "exercitation",
+        "duis",
+        "ex"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Felecia Mathis"
+        },
+        {
+          "id": 1,
+          "name": "Lula Dyer"
+        },
+        {
+          "id": 2,
+          "name": "Green Ortiz"
+        }
+      ],
+      "greeting": "Hello, Rivas Wagner! You have 7 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "658af4a78a30bcbd74cffc8e",
+      "index": 5,
+      "guid": "273acd2f-9522-4969-b247-f72c3faa72b6",
+      "isActive": false,
+      "balance": "$1,336.23",
+      "picture": "http://placehold.it/32x32",
+      "age": 36,
+      "eyeColor": "blue",
+      "name": "Fox Baker",
+      "gender": "male",
+      "company": "EGYPTO",
+      "email": "foxbaker@egypto.com",
+      "phone": "+1 (881) 481-2989",
+      "address": "862 Emerald Street, Dundee, Virgin Islands, 8267",
+      "about": "Ut qui minim labore fugiat aute id et pariatur ut adipisicing. Veniam aute in ad in ipsum et minim. Quis veniam culpa do Lorem.\r\n",
+      "registered": "2020-03-23T09:11:01 -04:00",
+      "latitude": -58.738207,
+      "longitude": 78.316942,
+      "tags": [
+        "elit",
+        "quis",
+        "quis",
+        "exercitation",
+        "reprehenderit",
+        "ea",
+        "sint"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Shepard Randall"
+        },
+        {
+          "id": 1,
+          "name": "Wolfe Bright"
+        },
+        {
+          "id": 2,
+          "name": "Nelda Johns"
+        }
+      ],
+      "greeting": "Hello, Fox Baker! You have 7 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "658af4a7faaf31ed291258f8",
+      "index": 6,
+      "guid": "fb134ae3-0f30-4d7a-b797-424931be2276",
+      "isActive": true,
+      "balance": "$3,054.97",
+      "picture": "http://placehold.it/32x32",
+      "age": 33,
+      "eyeColor": "blue",
+      "name": "Shannon Howard",
+      "gender": "male",
+      "company": "VIRXO",
+      "email": "shannonhoward@virxo.com",
+      "phone": "+1 (834) 451-2218",
+      "address": "134 Devon Avenue, Chilton, South Carolina, 4335",
+      "about": "Occaecat est adipisicing et ipsum eiusmod. Laborum quis tempor laborum dolore laboris ad eu cillum ut enim aute exercitation. Deserunt labore mollit exercitation cillum non nulla veniam id sunt ad qui. Magna veniam dolor duis officia elit officia mollit amet laborum deserunt pariatur occaecat.\r\n",
+      "registered": "2015-06-23T08:12:44 -03:00",
+      "latitude": 89.798972,
+      "longitude": -121.42249,
+      "tags": [
+        "ullamco",
+        "voluptate",
+        "laboris",
+        "ad",
+        "anim",
+        "ex",
+        "nulla"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Delia Ray"
+        },
+        {
+          "id": 1,
+          "name": "Nielsen White"
+        },
+        {
+          "id": 2,
+          "name": "Stein Lyons"
+        }
+      ],
+      "greeting": "Hello, Shannon Howard! You have 9 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]
